d99f969e4606445f0dc04fa48620f4cbd64ebd58
Atsidir <tamas.torjai@gmail.com>
2017-01-18 10:51:32 +0100

    new applicant new city added

--- example_data.py
+++ example_data.py
@@ -12,6 +12,7 @@ City.create(city_name='Szeged',location=1)
 City.create(city_name='Székesfehérvár',location=1)
 City.create(city_name='Debrecen',location=2)
 City.create(city_name='Warsaw',location=3)
+City.create(city_name='Budapest',location=1)
 
 
 Mentor.create(name='Matyi',school=1)
@@ -21,6 +22,7 @@ Mentor.create(name='Basil',school=3)
 Applicant.create(name='Panni',status='new',city='Szeged')
 Applicant.create(name='Ancsika',status='new',city='Debrecen')
 Applicant.create(name='Pancsika',status='new',city='Székesfehérvár')
+Applicant.create(name='Robi',status='new',applicant_id='as3A5',city='Budapest')
 
 InterviewSlot.create(start='2017-01-30 11:00',end='2017-01-30 12:00',reserved=False,assigned_mentor=1)
 InterviewSlot.create(start='2017-01-31 11:00',end='2017-01-30 12:00',reserved=False,assigned_mentor=1)
@@ -31,11 +33,34 @@ Interview.create(applicant=2,slot=2)
 Interview.create(applicant=3,slot=3)
 
 #query=Mentor.select(Mentor,InterviewSlot).join(InterviewSlot).where(InterviewSlot.reserved==True)
+
+"""
 query=Mentor.select(Mentor.name.alias('mentor_name'),InterviewSlot.start,Applicant.name,InterviewSlot.reserved).join(InterviewSlot).join(Interview).join(Applicant).naive()
 
 
 for item in query:
     print(item.mentor_name,item.name,item.start,item.reserved)
+"""
+
+query=Applicant.select(Applicant.name,Applicant.city,School.name.alias('school_name')).join(City,on=City.city_name==Applicant.city).join(School).naive()
+
+for applicant in query:
+    print(applicant.name,applicant.city,applicant.school_name)
+
+"""
+subquery = Tweet.select(fn.COUNT(Tweet.id)).where(Tweet.user == User.id)
+update = User.update(num_tweets=subquery)
+update.execute()
+
+"""
+
+subq=City.select(City.location).where(City.location==School.id)
+
+for item in subq:
+    print(City.city_name)
+
+#update=Applicant.update(school=subq)
+#update.execute()
 
 
 
